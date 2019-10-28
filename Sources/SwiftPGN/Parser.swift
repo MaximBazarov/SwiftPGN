4db8a2e0a60cbc546a4a5dffef2ec81f492ee5d9
vladislav zhukov <vzhukov@protonmail.ch>
2019-10-28 17:54:24 +0500

    updated parser

--- Sources/SwiftPGN/Parser.swift
+++ Sources/SwiftPGN/Parser.swift
@@ -12,21 +12,23 @@ extension PGN {
             case turn
             case whiteMove
             case blackMove
-            //case moveResult(NAG)
+            case moveResult
             case annotation
-            case annotation2
-            //case text(String)
-            //case arrow(from:SAN.Square, to: SAN.Square, color: HighlightColor)
-            //case squareHighlight(SAN.Square, HighlightColor)
+            case text
+            case arrow
+            case squareHighlight
         }
-        let text = text.replacingOccurrences(of: "\n", with: " ")
+        let text = text.replacingOccurrences(of: "\n", with: " ").replacingOccurrences(of: "...", with: ".")
         
         var counterparts: [Counterpart] = []
-        
         var currentText: String = ""
-        var operatingMode: OperatingMode = .none
-        var operatingModeBefore: OperatingMode = .none
         
+        var stackOfOperatingMode: [OperatingMode] = [.none]
+        var currentOperatingMode: OperatingMode {
+            assert(stackOfOperatingMode.count >= 1)
+            return stackOfOperatingMode.last ?? .none
+        }
+                
         var turn: TurnNumber?
         var turnCounterparts: [Counterpart] = []
          
@@ -35,73 +37,199 @@ extension PGN {
             switch String(symbol) {
             //TAG
             case "[":
-                if operatingMode == .none {
-                    operatingMode = .tagPair
+                if currentOperatingMode == .none {
+                    stackOfOperatingMode.append(.tagPair)
+                } else if currentOperatingMode == .annotation {
+                    stackOfOperatingMode.append(.squareHighlight)
+                } else {
+                     currentText += String(symbol)
                 }
             case "]":
-                if operatingMode == .tagPair {
+                if currentOperatingMode == .tagPair {
                     guard let tagPair = tagPair(from: currentText) else { return counterparts }
                     currentText = ""
-                    operatingMode = .none
+                    stackOfOperatingMode.removeLast()
                     counterparts.append(tagPair)
-                }                
+                } else if currentOperatingMode == .squareHighlight {
+                    stackOfOperatingMode.removeLast()
+                    currentText = ""
+                }
             //Comment
             case "{":
-                if operatingMode != .annotation2 {
-                    operatingModeBefore = operatingMode
-                    operatingMode = .annotation
-                } else {
-                    currentText += String(symbol)
-                }
+                stackOfOperatingMode.append(.annotation)
             case "}":
-                if operatingMode == .annotation {
-                    operatingMode = operatingModeBefore
+                if currentOperatingMode == .annotation {
+                    stackOfOperatingMode.removeLast()
                     counterparts.append(.text(currentText))
                     currentText = ""
-                } else {
-                    currentText += String(symbol)
                 }
             case "(":
-                operatingModeBefore = operatingMode
-                operatingMode = .annotation2
+                stackOfOperatingMode.append(.annotation)
             case ")":
-                operatingMode = operatingModeBefore
-                counterparts.append(.text(currentText))
-                currentText = ""
-                
+                if currentOperatingMode == .annotation {
+                    stackOfOperatingMode.removeLast()
+                    counterparts.append(.text(currentText))
+                    currentText = ""
+                }
             //Move
             case "1", "2", "3", "4", "5", "6", "7", "8", "9":
-                if operatingMode == .none {
+                if currentOperatingMode == .none || currentOperatingMode == .annotation {
                     currentText = ""
-                    operatingMode = .turn
-                }
+                    stackOfOperatingMode.append(.turn)
+                }  
                 currentText += String(symbol)
             case ".":
-                if operatingMode == .turn {
+                if currentOperatingMode == .turn {
+                    stackOfOperatingMode.removeLast()
                     turn = TurnNumber(currentText)
                     print(currentText)
                     currentText = ""
-                    operatingMode = .whiteMove
+                    stackOfOperatingMode.append(.whiteMove)
+                } else if currentOperatingMode == .blackMove {
+                    currentText = ""
                 } else {
                     currentText += String(symbol)
                 }
             case " ":
-                if operatingMode == .whiteMove {
-                    guard let move = SAN.move(of: currentText, color: SAN.Color.white) else { return counterparts }
+                if currentOperatingMode == .whiteMove {
+                    guard let move = SAN.move(of: currentText, color: SAN.Color.white) else {
+                        return counterparts
+                    }
                     turnCounterparts.append(.whiteMove(move))
                     
                     currentText = ""
-                    operatingMode = .blackMove
-                } else if operatingMode == .blackMove, currentText.isEmpty == false {
-                    guard let move = SAN.move(of: currentText, color: SAN.Color.black) else { return counterparts }
+                    stackOfOperatingMode.removeLast()
+                    stackOfOperatingMode.append(.blackMove)
+                } else if currentOperatingMode == .blackMove, currentText.isEmpty == false {
+                    guard let move = SAN.move(of: currentText, color: SAN.Color.black) else {
+                        return counterparts
+                    }
                     turnCounterparts.append(.blackMove(move))
                     
-                    guard let turn = turn else { return counterparts }
+                    guard let turn = turn else {
+                        return counterparts
+                    }
                     counterparts.append(.turn(turn, turnCounterparts))
                     turnCounterparts.removeAll()
                     
                     currentText = ""
-                    operatingMode = .none
+                    stackOfOperatingMode.removeLast()
+                } else {
+                    currentText += String(symbol)
+                }
+                
+            default:
+                currentText += String(symbol)
+            }
+        }
+        
+        return counterparts
+    }
+    
+    
+    public func parse2(pgn text: String) -> [Counterpart] {
+        enum OperatingMode {
+            case none
+            case turn
+            case whiteMove
+            case blackMove
+            case moveResult
+            case annotation
+            case text
+            case arrow
+            case squareHighlight
+        }
+        let text = text.replacingOccurrences(of: "\n", with: " ").replacingOccurrences(of: "...", with: ".")
+        
+        var counterparts: [Counterpart] = []
+        var currentText: String = ""
+        
+        var stackOfOperatingMode: [OperatingMode] = [.none]
+        var currentOperatingMode: OperatingMode {
+            assert(stackOfOperatingMode.count >= 1)
+            return stackOfOperatingMode.last ?? .none
+        }
+                
+        var turn: TurnNumber?
+        var turnCounterparts: [Counterpart] = []
+         
+        
+        for symbol in text {
+            switch String(symbol) {
+            //TAG
+            case "[":
+                if currentOperatingMode == .annotation {
+                    currentText = ""
+                } else {
+                    stackOfOperatingMode.append(.squareHighlight)
+                }
+            case "]":
+                if currentOperatingMode == .squareHighlight {
+                    print(currentText)
+                    currentText = ""
+                }
+            //Comment
+            case "{":
+                stackOfOperatingMode.append(.annotation)
+            case "}":
+                if currentOperatingMode == .annotation {
+                    print(parse2(pgn: currentText))
+                    currentText = ""
+                    stackOfOperatingMode.removeLast()
+                }
+            case "(":
+                stackOfOperatingMode.append(.annotation)
+            case ")":
+                if currentOperatingMode == .annotation {
+                    print(parse2(pgn: currentText))
+                    currentText = ""
+                    stackOfOperatingMode.removeLast()
+                }
+            //Move
+            case "1", "2", "3", "4", "5", "6", "7", "8", "9":
+                if currentOperatingMode == .none  {
+                    currentText = ""
+                    stackOfOperatingMode.append(.turn)
+                }
+                currentText += String(symbol)
+            case ".":
+                if currentOperatingMode == .turn {
+                    stackOfOperatingMode.removeLast()
+                    turn = TurnNumber(currentText)
+                    print(currentText)
+                    currentText = ""
+                    stackOfOperatingMode.append(.whiteMove)
+                } else if currentOperatingMode == .blackMove {
+                    currentText = ""
+                } else {
+                    currentText += String(symbol)
+                }
+            case " ":
+                if currentOperatingMode == .whiteMove {
+                    guard let move = SAN.move(of: currentText, color: SAN.Color.white) else {
+                        return counterparts
+                    }
+                    turnCounterparts.append(.whiteMove(move))
+                    
+                    currentText = ""
+                    stackOfOperatingMode.removeLast()
+                    stackOfOperatingMode.append(.blackMove)
+                } else if currentOperatingMode == .blackMove, currentText.isEmpty == false {
+                    guard let move = SAN.move(of: currentText, color: SAN.Color.black) else {
+                        return counterparts
+                    }
+                    turnCounterparts.append(.blackMove(move))
+                    
+                    guard let turn = turn else {
+                        return counterparts
+                    }
+                    counterparts.append(.turn(turn, turnCounterparts))
+                    turnCounterparts.removeAll()
+                    
+                    currentText = ""
+                    stackOfOperatingMode.removeLast()
+                } else {
+                    currentText += String(symbol)
                 }
                 
             default:
--- Tests/SwiftPGNTests/ParserTest.swift
+++ Tests/SwiftPGNTests/ParserTest.swift
@@ -41,25 +41,56 @@ class ParserTest: XCTestCase {
         [Result "1/2-1/2"]
         """
         
-        let counterparts = PGN("")?.parse(pgn: pgn)
+        let pgn2 = """
+        1.e4 e5 2.Nf3 Nc6 3.Nc3 Nf6 4.Bb5 Bc5 5.Nxe5 Re8 6.Nxe5 Re8 7.Nxc6 dxc6 8.Bc4 b5
+        9.Be2 Nxe4 10.Nxe4 Rxe4 11.Bf3 Re6 12.c3 Qd3 13.b4 Bb6 14.a4 bxa4 15.Qxa4 Bd7
+        16.Ra2 Rae8 17.Qa6
+        {[#][%csl Yf3][%cal Rd3f3,Re6g6,Gg6g1] Morphy took twelve minutes
+        over his next move, probably to assure himself that the combination was sound
+        and that he had a forced win in every variation.}
+        17...Qxf3 !! 18.gxf3 Rg6+ 19.Kh1 Bh3 20.Rd1 ({Not} 20.Rg1 Rxg1+ 21.Kxg1 Re1+ -+)
+        20...Bg2+ 21.Kg1 Bxf3+ 22.Kf1 Bg2+
+        (22...Rg2 ! {would have won more quickly. For instance:} 23.Qd3 Rxf2+
+        24.Kg1 Rg2+ 25.Kh1 Rg1#)
+        23.Kg1 Bh3+ 24.Kh1 Bxf2 25.Qf1 {Absolutely forced.} 25...Bxf1 26.Rxf1 Re2
+        27.Ra1 Rh6 28.d4 Be3 0-1
+        """
+        
+        let counterparts = PGN("")?.parse2(pgn: pgn)
         
         XCTAssertNotNil(counterparts)
         
-        let tags = ["Event", "Site", "Date", "Round", "White", "Black", "Result"]
-        let texts = ["F/S Return Match", "Belgrade, Serbia JUG", "1992.11.04", "29", "Fischer, Robert J.", "Spassky, Boris V.", "1/2-1/2"]
+        //let tags = ["Event", "Site", "Date", "Round", "White", "Black", "Result"]
+        //let texts = ["F/S Return Match", "Belgrade, Serbia JUG", "1992.11.04", "29", "Fischer, Robert J.", "Spassky, Boris V.", "1/2-1/2"]
         
+
 //        for (index, part) in counterparts!.enumerated() {
 //            switch part {
 //            case .tagPair(let tag, let text):
 //                print("\(tag.rawValue) -> \(text)")
 //                XCTAssert(text == texts[index])
 //                XCTAssert(tag == PGN.Tag(rawValue: tags[index]))
-//                
-//            default: XCTFail()
+//
+//            default: break //XCTFail()
 //            }
 //        }
         
         for part in counterparts! {
+            switch part {
+            case .turn(let turn, let parts):
+                print(turn)
+                for item in parts {
+                    switch item {
+                    case .whiteMove(let move):
+                        print(move.hashValue)
+                    case .blackMove(let move):
+                        print(move.hashValue)
+                    default: break
+                    }
+                }
+            default: break
+            }
+            
             print(part)
         }
     }
